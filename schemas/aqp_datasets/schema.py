55b45f333ff685a072ac3690e1fc5f46f8b47aea
Aaron Hurst <ahhurst0@gmail.com>
2023-02-11 18:36:59 +0100

    Changed method of getting dataset schemas

--- schemas/aqp_datasets/schema.py
+++ schemas/aqp_datasets/schema.py
@@ -1,29 +1,18 @@
+import json
+
 from ensemble_compilation.graph_representation import SchemaGraph, Table
 
 
 def get_schema(data_source, dataset_id, csv_path):
-    if (data_source == "uci") and (dataset_id == "household_power_consumption"):
-        return get_household_power_consumption_schema(csv_path)
-    else:
-        raise ValueError("Invalid dataset.")
-
-
-def get_household_power_consumption_schema(csv_path):
+    with open("schemas.json", "r") as fp:
+        schemas = json.load(fp)
     schema = SchemaGraph()
     schema.add_table(
         Table(
-            "household_power_consumption",
-            attributes=[
-                "global_active_power",
-                "global_reactive_power",
-                "voltage",
-                "global_intensity",
-                "sub_metering_1",
-                "sub_metering_2",
-                "sub_metering_3",
-            ],
+            dataset_id,
+            attributes=schemas[data_source][dataset_id]["column_names"],
             csv_file_location=csv_path,
-            table_size=2049280,
+            table_size=schemas[data_source][dataset_id]["n_rows"],
         )
     )
     return schema
