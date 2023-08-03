39389ed4ede15f4eed0cbab08ea01f29fff0f21b
Aaron Hurst <ahhurst0@gmail.com>
2023-08-03 16:47:00 +0200

    Get number of rows accurately, not from schema

--- schemas/aqp_datasets/schema.py
+++ schemas/aqp_datasets/schema.py
@@ -5,14 +5,19 @@ from ensemble_compilation.graph_representation import SchemaGraph, Table
 
 from config import DATA_DIR
 
-SUFFIXES = ["_10m", "_100m", "_1b"]
+SUFFIXES = {"_10m": 10000000, "_100m": 100000000, "_1b": 1000000000}
 
 
 def get_schema(dataset_id, csv_path):
     """Return SchemaGraph object for dataset based on JSON schema file."""
     dataset_id_no_suffix = dataset_id
     for suffix in SUFFIXES:
-        dataset_id_no_suffix = dataset_id_no_suffix.removesuffix(suffix)
+        if suffix in dataset_id:
+            dataset_id_no_suffix = dataset_id_no_suffix.removesuffix(suffix)
+            n_rows = SUFFIXES[suffix]
+    if not n_rows:
+        dataset_id_no_suffix = dataset_id
+        n_rows = sum(1 for _ in open(csv_path)) - 1  # excludes header
     filepath = os.path.join(DATA_DIR, "schemas", "aqp", f"{dataset_id_no_suffix}.json")
     with open(filepath, "r") as f:
         schema = json.load(f)
@@ -22,7 +27,7 @@ def get_schema(dataset_id, csv_path):
             dataset_id.replace("-", "_"),
             attributes=schema["column_names"],
             csv_file_location=csv_path,
-            table_size=schema["n_rows"],
+            table_size=n_rows,
         )
     )
     return schema_graph, schema
